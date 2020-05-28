89ada6cfac0fe43ffa9f25c3a0427f4247ec6c7e
yedward <yedward92@qq.com>
2020-05-28 12:07:48 +0800

    modify

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 )
 
-const webContent = "Hello World!"
+const webContent = "Hello, World!"
 
 func main() {
 	http.HandleFunc("/", helloHandler)
